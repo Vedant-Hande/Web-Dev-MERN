fa7b40722d4058258533946348131e25c67cee3e
Vedant-Hande <148268306+Vedant-Hande@users.noreply.github.com>
2025-03-06 17:21:38 +0530

    rename file scrpt.js to console_practice.js and crete 1 new file named as script.js

---
+++ JAVASCRIPT/console_practice.js
@@ -0,0 +1,61 @@
+// STRING IN JAVASCRIPT
+
+let name= "Vedant Hande";
+undefined
+
+// let len= name.length();
+// VM188:1 Uncaught TypeError: name.length is not a function
+//     at <anonymous>:1:15
+// (anonymous) @ VM188:1Understand this errorAI
+
+let len= name.length;
+undefined
+
+len
+12
+
+
+name
+'Vedant Hande'
+
+let fChar = name[0];
+undefined
+
+fChar
+'V'
+
+
+let lChar= name[10];
+undefined
+
+lChar
+'d'
+
+let l_Char= name[name.length-1];
+undefined
+
+l_Char
+'e'
+
+//  finding the output of this  vedant Hande+123
+let newStr= "vedant Hande";
+undefined
+
+newStr+123
+'vedant Hande123'
+
+
+let emptyStr="";
+undefined
+
+
+emptyStr.length
+0
+
+let spaceStr=" ";
+undefined
+
+
+spaceStr.length
+1
+
