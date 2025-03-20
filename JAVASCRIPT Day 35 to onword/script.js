80701003415e64a037b524ef826e8366cfcee35a
vedant hande <vedanthande453@gmail.com>
2025-03-20 17:36:41 +0530

    Uinary Operator

--- JAVASCRIPT Day 35 to onword/script.js
+++ JAVASCRIPT Day 35 to onword/script.js
@@ -1,9 +1,4 @@
-// let a = 5;
-// console.log(a++);
-// console.log(++a);
-// console.log(-a);
-// console.log(!a);
-
+console.log("DAY 45");
 // ------------------------ Templet Retrals ----------------------------------
 
 let pen1_Price = 10;
@@ -12,3 +7,20 @@ let pen2_Price = 20;
 let output = `The total price is : ${pen1_Price + pen2_Price} Rupess.`;
 // back tik
 console.log(output);
+
+// --------------------- Arithmetic Operator ------------------------------
+
+let a = 10;
+let b = 2;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+
+//----------------------- Uinary Operator ------------------------------------
+
+console.log(a++); //10
+console.log(a++); //11
+console.log(++a); //13
