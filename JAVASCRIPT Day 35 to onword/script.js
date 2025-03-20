dd946703a22979f6aeb8d7ea7a29b4bed055f51a
vedant hande <vedanthande453@gmail.com>
2025-03-20 18:40:09 +0530

    conditional Operator

--- JAVASCRIPT Day 35 to onword/script.js
+++ JAVASCRIPT Day 35 to onword/script.js
@@ -24,3 +24,16 @@ console.log(a ** b);
 console.log(a++); //10
 console.log(a++); //11
 console.log(++a); //13
+
+// --------------------- Conditional Statement ------------------------------
+
+let color = "yellow";
+if (color == "red") {
+  console.log("STOP...");
+}
+if (color == "yellow") {
+  console.log("SLOW DOWN...");
+}
+if (color == "green") {
+  console.log("YOU CAN GO NOW...");
+}
