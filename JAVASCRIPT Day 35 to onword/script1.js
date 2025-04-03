d516f845815642be52c4d5bde19685ce1aebbe64
vedant hande <vedanthande453@gmail.com>
2025-04-03 21:44:02 +0530

    Array practice Question

--- JAVASCRIPT Day 35 to onword/script1.js
+++ JAVASCRIPT Day 35 to onword/script1.js
@@ -32,4 +32,64 @@ start.unshift("july");
 start.unshift("june");
 console.log(start);
 
+console.log(
+  "------------------------DAY 50----------------------------------------"
+);
 
+const arr = [
+  [2, 4],
+  [3, 6],
+  [4, 8],
+];
+undefined;
+console.log(arr);
+// VM144:1 (3) [Array(2), Array(2), Array(2)]0: (2) [2, 4]1: (2) [3, 6]2: (2) [4, 8]length: 3[[Prototype]]: Array(0)
+undefined;
+arr[0];
+// (2) [2, 4]
+arr.length;
+3;
+// practice Question
+undefined;
+let pr1 = [7, 9, 0, -2];
+undefined;
+let n = 3;
+// undefined
+console.log(pr1.slice(0, n));
+// VM475:1 (3) [7, 9, 0]
+// undefined
+console.log(pr1.slice(3));
+// VM496:1 [-2]
+// undefined
+console.log(pr1.slice(pr1.length - n));
+// VM534:1 (3) [9, 0, -2]
+// undefined
+const pr_Str = "vedant";
+// undefined
+if (pr_Str == null) {
+  console.log("string is empty");
+} else {
+}
+// undefined
+if (pr_Str == null) {
+  console.log("string is empty");
+} else {
+  console.log("string is not empty");
+}
+// VM762:4 string is not empty
+// undefined
+if (pr_Str == "") {
+  console.log("string is empty");
+} else {
+  console.log("string is not empty");
+}
+// VM770:4 string is not empty
+// undefined
+if (pr_Str.length == 0) {
+  console.log("string is empty");
+} else {
+  console.log("string is not empty");
+}
+// VM791:4 string is not empty
+// undefined
+const pr_Char = "V";
