11ee43cb10224340eb3c8cb5cbda9ac03f5d1e35
vedant hande <vedanthande453@gmail.com>
2025-04-04 17:19:18 +0530

    fav_Movie guessing game completed

--- JAVASCRIPT Day 35 to onword/script1.js
+++ JAVASCRIPT Day 35 to onword/script1.js
@@ -230,13 +230,12 @@ for (let i = num; i <= num * 10; i = i + num) {
 // VM256:3 70
 // undefined
 
-
-// fav_Movie guessing game 
+// fav_Movie guessing game
 console.log("Game started press quit to exit the game !");
 
 const fav_Movie = "war";
 let guess = prompt("Guess my fav movie ...");
-while (guess !== fav_Movie && guess !== "quit") {
+while (guess != fav_Movie && guess != "quit") {
   guess = prompt(" Wrong guess! Plz try Again");
 }
 if (guess == fav_Movie) {
