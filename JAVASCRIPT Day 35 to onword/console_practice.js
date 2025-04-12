57004ffa4326671f9cfbd84ef5d8b44797c13552
vedant hande <vedanthande453@gmail.com>
2025-04-12 21:41:27 +0530

    commit

--- JAVASCRIPT Day 35 to onword/console_practice.js
+++ JAVASCRIPT Day 35 to onword/console_practice.js
@@ -3,11 +3,6 @@
 let name = "Vedant Hande";
 undefined;
 
-// let len= name.length();
-// VM188:1 Uncaught TypeError: name.length is not a function
-//     at <anonymous>:1:15
-// (anonymous) @ VM188:1Understand this errorAI
-
 let len = name.length;
 undefined;
 
