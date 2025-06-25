0d41a5e9732e7c066f2ad6744fe6bb5582bac58f
vedant hande <vedanthande453@gmail.com>
2025-06-25 23:11:59 +0530

    Fix text content assignment and enhance image styling in script.js

--- Javascript part - 9 dom basic/script.js
+++ Javascript part - 9 dom basic/script.js
@@ -1,9 +1,17 @@
 const mainImg = document.getElementById("mainImg");
 const spider = document.getElementById("spider");
 mainImg.src = "assets/creation_1.png";
-spider.textContent = "This is a spider";
+spider.innerText = "This is a spider";
 
 let smallImgs = document.getElementsByClassName("oldImg");
 for (let i = 0; i < smallImgs.length; i++) {
-  smallImgs[i].src = "assets/spiderman_img.png";
+  //   smallImgs[i].style.border = "2px solid red";
+  smallImgs[i].style.width = "100px";
+  smallImgs = smallImgs[i].src = "assets/spiderman_img.png";
 }
+
+let demoPragraph = document.getElementsByTagName("p");
+demoPragraph[0].style.color = "red";
+demoPragraph[1].style.backgroundColor = "yellow";
+
+// query selector
