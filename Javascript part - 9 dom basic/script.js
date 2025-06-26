8d6d2f98cfcddb6f449ebde02d6bc9ea39cbeca2
vedant hande <vedanthande453@gmail.com>
2025-06-26 21:25:29 +0530

    Update HTML structure and enhance JavaScript functionality for improved content display and styling

--- Javascript part - 9 dom basic/script.js
+++ Javascript part - 9 dom basic/script.js
@@ -5,8 +5,6 @@ spider.innerText = "This is a spider";
 
 let smallImgs = document.getElementsByClassName("oldImg");
 for (let i = 0; i < smallImgs.length; i++) {
-  //   smallImgs[i].style.border = "2px solid red";
-  smallImgs[i].style.width = "100px";
   smallImgs = smallImgs[i].src = "assets/spiderman_img.png";
 }
 
@@ -15,3 +13,24 @@ demoPragraph[0].style.color = "red";
 demoPragraph[1].style.backgroundColor = "yellow";
 
 // query selector
+let description = document.querySelector("#description");
+description.innerHTML =
+  "<b>lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum</b>.";
+
+let newImg = document.querySelectorAll(".images img");
+// let newImg = document.querySelectorAll(".images .oldImg");
+
+for (const element of newImg) {
+  element.src = " assets/creation_3.jpeg";
+}
+
+let changeHeading = document.querySelector("#heading2Id");
+console.log(changeHeading.getAttribute("class"));
+console.log(changeHeading.getAttribute("id"));
+console.log(changeHeading.setAttribute("id", "heading_2new"));
+console.log(changeHeading.getAttribute("id"));
+
+let changeColor = document.querySelectorAll(".box a");
+for (const element of changeColor) {
+  element.style.color = "green";
+}
