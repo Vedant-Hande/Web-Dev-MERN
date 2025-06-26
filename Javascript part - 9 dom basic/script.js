51dc7e1a0339a7d182c3a3e6b3f69a403f8f8723
vedant hande <vedanthande453@gmail.com>
2025-06-26 22:58:57 +0530

    Enhance HTML structure and add dynamic content creation in JavaScript for improved interactivity

--- Javascript part - 9 dom basic/script.js
+++ Javascript part - 9 dom basic/script.js
@@ -34,3 +34,44 @@ let changeColor = document.querySelectorAll(".box a");
 for (const element of changeColor) {
   element.style.color = "green";
 }
+
+const mainBody = document.querySelector("body");
+
+// practice question
+// 1 ->
+const addP = document.createElement("p");
+addP.innerText = "Hey I'm red";
+addP.style.color = "red";
+mainBody.append(addP);
+
+// 2 ->
+const addDiv = document.createElement("div");
+addDiv.style.backgroundColor = "pink";
+addDiv.style.border = "2px solid black";
+mainBody.append(addDiv);
+
+const addH1 = document.createElement("h1");
+addH1.innerText = "hey i'm a H1";
+addDiv.appendChild(addH1);
+const addp = document.createElement("p");
+addp.innerText = "me too";
+// addDiv.appendChild(addp); we can also use it
+addH1.insertAdjacentElement("afterend", addp);
+
+// Assignment Questions
+const newBtn = document.createElement("input");
+newBtn.setAttribute("type", "buttton");
+newBtn.setAttribute("id", "newBtn");
+newBtn.setAttribute("value", "Click Me");
+newBtn.setAttribute("placeholder", "Username");
+// newBtn.toggleAttribute("id", "newBtn");
+newBtn.setAttribute("id", "btn");
+mainBody.append(newBtn);
+
+let accessBtn = document.querySelector("#btn");
+accessBtn.style.backgroundColor = "blue";
+accessBtn.style.color = "yellow";
+
+const prar1 = document.createElement("p");
+prar1.innerHTML = "<b>DELTA 6.0 </b> Apna college";
+mainBody.append(prar1);
