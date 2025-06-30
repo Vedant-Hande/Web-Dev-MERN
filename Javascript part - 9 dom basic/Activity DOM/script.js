0a6709261189975523df47994293cefc6cc3b9d1
vedant hande <vedanthande453@gmail.com>
2025-06-30 21:44:08 +0530

    Refactor code structure and remove redundant sections for improved readability and maintainability

---
+++ Javascript part - 9 dom basic/Activity DOM/script.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector("button");
+const h2 = document.querySelector("h2");
+const div1 = document.querySelector("div");
+
+btn.addEventListener("click", function () {
+  let randomColor = getRandomColor();
+  h2.innerText = randomColor;
+  div1.style.backgroundColor = randomColor;
+});
+
+function getRandomColor() {
+  let red = Math.floor(Math.random() * 255);
+  let green = Math.floor(Math.random() * 255);
+  let blue = Math.floor(Math.random() * 255);
+
+  let result = `rgb(${red}, ${green}, ${blue})`;
+  return result;
+}
