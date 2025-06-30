0322bb8057658f123dc3b0f4cf612ff96992b85f
vedant hande <vedanthande453@gmail.com>
2025-06-30 22:00:53 +0530

    Add event parameter to button click handler and log event details; remove redundant border style from button

--- Javascript part - 9 dom basic/Activity DOM/script.js
+++ Javascript part - 9 dom basic/Activity DOM/script.js
@@ -2,10 +2,12 @@ const btn = document.querySelector("button");
 const h2 = document.querySelector("h2");
 const div1 = document.querySelector("div");
 
-btn.addEventListener("click", function () {
+btn.addEventListener("click", function (event) {
+  console.dir(event);
   let randomColor = getRandomColor();
   h2.innerText = randomColor;
   div1.style.backgroundColor = randomColor;
+  this.style.backgroundColor = randomColor;
 });
 
 function getRandomColor() {
