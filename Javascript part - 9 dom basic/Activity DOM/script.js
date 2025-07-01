34a30299c3bd4eaba3f42307f0261452e56ec1ed
vedant hande <vedanthande453@gmail.com>
2025-07-01 22:18:43 +0530

    Implement task addition and deletion functionality in the todo list

--- Javascript part - 9 dom basic/Activity DOM/script.js
+++ Javascript part - 9 dom basic/Activity DOM/script.js
@@ -2,7 +2,7 @@ const btn = document.querySelector("button");
 const h2 = document.querySelector("h2");
 const div1 = document.querySelector("div");
 
-btn.addEventListener("click", function (event) {
+btn.addEventListener("dbclick", function (event) {
   console.dir(event);
   let randomColor = getRandomColor();
   h2.innerText = randomColor;
@@ -36,4 +36,31 @@ form.addEventListener("input", function (event) {
   console.dir(this[0]);
   console.log(this[0].value);
   console.log(this[1].value);
+});
+
+const addBtn = document.querySelector(".addtsk");
+const inpTask = document.querySelector("input");
+const ul = document.querySelector("ul");
+
+addBtn.addEventListener("click", function () {
+  let delbtn = document.createElement("button");
+  let newli = document.createElement("li");
+
+  newli.innerText = inpTask.value;
+  delbtn.innerText = "delete";
+  //adding class to the delBtn using class list
+  delbtn.classList.add("delete");
+  newli.appendChild(delbtn);
+
+  ul.appendChild(newli);
+  inpTask.innerText = "";
+});
+
+// if we trigger the event on the delete button then it cant work on new ly create li button so we directly add eventlistener to the the ul *target -> which element was targeted by the event listener *
+
+ul.addEventListener("click", function (event) {
+  if (event.target.nodeName == "BUTTON") {
+    let remLi = event.target.parentElement;
+    remLi.remove();
+  }
 });
