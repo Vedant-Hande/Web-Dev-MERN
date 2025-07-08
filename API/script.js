cef5f2621a0812ac0c9b65e7d5ccf609a97feaa9
vedant hande <vedanthande453@gmail.com>
2025-07-08 21:32:07 +0530

    Refactor script.js to implement async/await for data fetching, enhancing readability and error handling. Comment out previous fetch logic for clarity and add a loop to fetch random user data multiple times.

--- API/script.js
+++ API/script.js
@@ -1,18 +1,46 @@
 let url = "https://www.reddit.com/r/Wallstreetbets/top.json";
-fetch(url)
-  .then((res) => {
-    return res.json();
-  })
-  .then((data) => {
-    console.log(data);
-    return fetch(url);
-  })
-  .then((res) => {
-    return res.json();
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// fetch(url)
+//   .then((res) => {
+//     return res.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//     return fetch(url);
+//   })
+//   .then((res) => {
+//     return res.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// using async and await
+
+async function getData() {
+  try {
+    let d2 = await fetch(url);
+    console.log("d2 :>> ", d2);
+    let d2Ans = await d2.json();
+    console.log("d2Ans :>> ", d2Ans);
+  } catch (err) {
+    console.log("err :>> ", err);
+  }
+}
+getData();
+
+let url1 = "https://randomuser.me/api/";
+for (let i = 1; i <= 5; i++) {
+  async function getData() {
+    try {
+      let data = await fetch(url1);
+      let dataAns = await data.json();
+      console.log("data of " + i + "is >> ", dataAns.results[0]);
+    } catch (err) {
+      console.log("err :>> ", err);
+    }
+  }
+  getData();
+}
