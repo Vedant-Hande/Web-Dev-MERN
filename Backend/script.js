2986f1b2d3c3b41c1cd6d5b8570a3138392172ba
vedant hande <vedanthande453@gmail.com>
2025-07-15 22:30:39 +0530

    Backend learning

---
+++ Backend/script.js
@@ -0,0 +1,5 @@
+let math = require("./math");
+console.log("sum is =>> ", math.sum(2, 4));
+console.log("sub is =>> ", math.sub(16, 4));
+console.log("mul is =>> ", math.mul(2, 4));
+console.log("div is =>> ", math.sum(2, 4));
