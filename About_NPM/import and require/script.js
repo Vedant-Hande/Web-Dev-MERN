ad32a5d30aea29ed5eec639c98af9a61b2088d7f
vedant hande <vedanthande453@gmail.com>
2025-07-16 21:19:46 +0530

    Install random-words package and learn about import

---
+++ About_NPM/import and require/script.js
@@ -0,0 +1,6 @@
+import { generate } from "random-words";
+console.log(generate(10));
+
+// 1. import save the memory space
+// we can import form the file whatever we need no need to import full file
+// 3. use only 1 at a time require or inport
