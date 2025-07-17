41b11c0bd4cd9f117a4c8b4f2c4883070ae33718
vedant hande <vedanthande453@gmail.com>
2025-07-17 23:05:57 +0530

    day 3 Backend  - leaning {path param & query string}

--- Backend/express/index.js
+++ Backend/express/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+// const open = require("open");
+
 const app = express();
 
 const port = 3000;
@@ -6,6 +8,7 @@ app.listen(port, () => {
   console.log("server is listening", port);
 });
 
+// root ## http://localhost:3000/
 app.get("/", (req, res) => {
   res.send({
     name: "vedant",
@@ -15,13 +18,34 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/car", (req, res) => {
-  const cars = ["BMW", "Audi", "Mercedes", "Toyota", "Honda"];
-  const carList = cars.map((car) => `<li>${car}</li>`).join("");
-  res.send(`<h1>Cars</h1><ul>${carList}</ul>`);
-});
+// query String   ## localhost:3000/search?q=search_query
 
-// 404 Not Found Handler
-app.use((req, res) => {
+http: app.get("/search", (req, res) => {
+  let { q } = req.query;
+
+  if (q) {
+    if (q === "cars") {
+      const cars = ["BMW", "Audi", "Mercedes", "Toyota", "Honda"];
+      const carList = cars.map((car) => `<li>${car}</li>`).join("");
+      res.send(
+        `<h1>welcome to the page of @${q}</h1><h2>Cars</h2><h3><ul>${carList}</ul></h3> `
+      );
+    } else {
+      res.send(`<h1>welcome to the page of @${q}</h1>`);
+    }
+  }
   res.status(404).json({ error: "Route not found" });
 });
+
+// Path Param ## http://localhost:3000/vedant/123
+
+app.get("/:user/:id", (req, res) => {
+  let { user, id } = req.params;
+  console.log(req.params, "  user->" + user, "&", "id->" + id);
+  res.send(`<h1>welcome to the page of @${user}</h1>`);
+});
+
+// // 404 Not Found Handler
+// app.use((req, res) => {
+//   res.status(404).json({ error: "Route not found" });
+// });
