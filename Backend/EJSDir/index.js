aa177a9c5f50f81ef882fcbce4ddc10c7e023ed9
vedant hande <vedanthande453@gmail.com>
2025-07-19 23:54:52 +0530

    add roll dice EJS page and extend user profiles in data.json

--- Backend/EJSDir/index.js
+++ Backend/EJSDir/index.js
@@ -4,7 +4,9 @@ const instaData = require("./data.json");
 const app = express();
 
 const port = 3000;
+
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.listen(port, () => {
   console.log("server is listening", port);
@@ -14,7 +16,7 @@ app.set("views", path.join(__dirname, "/views"));
 
 app.get("/ig/:account", (req, res) => {
   let { account } = req.params;
-  console.log(instaData[account]);
+  // console.log(instaData[account]);
   res.render("home.ejs", { data: instaData[account] });
 });
 
@@ -23,4 +25,13 @@ app.get("/", (req, res) => {
   res.render("rolldise.ejs", { roll: rollDise });
   // res.render("rollDise.ejs", { rollDise: rollDise });
   // res.render("rollDise.ejs", { dise: rollDise });
+});
+
+// 404 Not Found Handler
+app.use((req, res) => {
+  if (res.status(404)) {
+    res.render("error.ejs", {
+      stat: "page not found",
+    });
+  }
 });
