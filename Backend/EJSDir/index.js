96bfef553dd54281077faca9815835d99f4ddbdc
vedant hande <vedanthande453@gmail.com>
2025-07-19 13:48:35 +0530

    done

--- Backend/EJSDir/index.js
+++ Backend/EJSDir/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const instaData = require("./data.json");
 const app = express();
 
 const port = 3000;
@@ -11,9 +12,15 @@ app.listen(port, () => {
 
 app.set("views", path.join(__dirname, "/views"));
 
-app.get("/", (req, res) => {
-  let rollDise = Math.ceil(Math.random() * 6);
-  res.render("home.ejs", { rollDise });
-  // res.render("home.ejs", { rollDise: rollDise });
-  // res.render("home.ejs", { dise: rollDise });
+app.get("/ig/:account", (req, res) => {
+  let { account } = req.params;
+  console.log(instaData[account]);
+  res.render("home.ejs", { data: instaData[account] });
 });
+
+// app.get("/", (req, res) => {
+//   let rollDise = Math.ceil(Math.random() * 6);
+//   // res.render("home.ejs", { rollDise });
+//   // res.render("home.ejs", { rollDise: rollDise });
+//   // res.render("home.ejs", { dise: rollDise });
+// });
