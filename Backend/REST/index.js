a3334448e95eeee2e86a93673c8944d33f0dcc7a
vedant hande <vedanthande453@gmail.com>
2025-07-23 01:17:22 +0530

    Add EJS rendering for posts and create initial CSS styles

--- Backend/REST/index.js
+++ Backend/REST/index.js
@@ -1,3 +1,4 @@
+const { name } = require("ejs");
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -6,10 +7,48 @@ const port = 3000;
 
 app.set("view engine", "views");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname, "public")));
 
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
+let posts = [
+  {
+    name: "John Doe",
+    title: "My First Post",
+    content: "This is the content of my first post.",
+  },
+  {
+    name: "John Doe",
+    title: "My First Post",
+    content: "This is the content of my first post.",
+  },
+  {
+    name: "John Doe",
+    title: "My First Post",
+    content: "This is the content of my first post.",
+  },
+  {
+    name: "John Doe",
+    title: "My First Post",
+    content: "This is the content of my first post.",
+  },
+];
 app.listen(port, () => {
   console.log("app is running on port " + port);
 });
 
-app.get("/")
+app.get("/posts", (req, res) => {
+  // res.send("server is running");
+  res.render("index.ejs", { posts });
+});
+
+app.get("/posts/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+app.post("/posts", (req, res) => {
+  let { name, title, content } = req.body;
+  posts.push({ name, title, content });
+  res.render("index.ejs", { posts });
+});
+
