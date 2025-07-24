7b183b1589e018fb53f7185b91f49363263d882f
vedant hande <vedanthande453@gmail.com>
2025-07-24 23:55:16 +0530

    feat: add method-override package with debug and ms dependencies
    - Added method-override package to handle HTTP verb overrides.
    - Included debug package for logging purposes.
    - Integrated ms package for time duration parsing and formatting.
    - Created Karma configuration for testing with Mocha, Chai, and Sinon.
    - Implemented browser and Node.js specific debug implementations.
    - Added edit.ejs view for editing posts with form submission.

--- Backend/REST/index.js
+++ Backend/REST/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
+const methodOverride = require("method-override");
 
 const port = 3000;
 
@@ -10,6 +11,7 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 let posts = [
   {
@@ -63,4 +65,18 @@ app.get("/posts/:id", (req, res) => {
   let post = posts.find((p) => id === p.id);
   console.log(post);
   res.render("show.ejs", { post: post });
+});
+
+app.patch("/posts/:id", (req, res) => {
+  let { id } = req.body;
+  if (posts.find((p) => id === p.id)) {
+    res.redirect("/posts");
+  }
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+  console.log(post);
+  res.render("edit.ejs", { post });
 });
