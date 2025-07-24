0033299625a8a627291de860cb29ea2c8d6a2950
vedant hande <vedanthande453@gmail.com>
2025-07-25 00:37:00 +0530

    feat: implement CRUD operations for posts with delete functionality

--- Backend/REST/index.js
+++ Backend/REST/index.js
@@ -9,6 +9,7 @@ const port = 3000;
 app.set("view engine", "views");
 app.set("views", path.join(__dirname, "views"));
 
+//middlewares->
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
@@ -44,14 +45,17 @@ app.listen(port, () => {
   console.log("app is running on port " + port);
 });
 
+//root
 app.get("/posts", (req, res) => {
   res.render("index.ejs", { posts });
 });
 
+// creating new post
 app.get("/posts/new", (req, res) => {
   res.render("new.ejs");
 });
 
+// form sending request to post --new.ejs
 app.post("/posts", (req, res) => {
   let { name, title, content } = req.body;
   let id = uuidv4(); // Generate a unique ID for the post
@@ -59,6 +63,7 @@ app.post("/posts", (req, res) => {
   res.redirect("/posts");
 });
 
+//detailed post show
 app.get("/posts/:id", (req, res) => {
   let { id } = req.params;
   console.log(id);
@@ -67,11 +72,17 @@ app.get("/posts/:id", (req, res) => {
   res.render("show.ejs", { post: post });
 });
 
+// updating data of post {patch req by form -- edit.ejs}
 app.patch("/posts/:id", (req, res) => {
-  let { id } = req.body;
-  if (posts.find((p) => id === p.id)) {
-    res.redirect("/posts");
-  }
+  let { id } = req.params;
+  let newName = req.body.name;
+  let newTitle = req.body.title;
+  let newContent = req.body.content;
+  let post = posts.find((p) => id === p.id);
+  post.name = newName;
+  post.title = newTitle;
+  post.content = newContent;
+  res.redirect("/posts");
 });
 
 app.get("/posts/:id/edit", (req, res) => {
@@ -79,4 +90,11 @@ app.get("/posts/:id/edit", (req, res) => {
   let post = posts.find((p) => id === p.id);
   console.log(post);
   res.render("edit.ejs", { post });
+});
+
+// Deleting post
+app.delete("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  posts = posts.filter((p) => id != p.id);
+  res.redirect("/posts");
 });
