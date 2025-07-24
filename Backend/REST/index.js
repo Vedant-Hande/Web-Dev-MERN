113b764949278c70579f58858e4ecc4a6e895e0a
vedant hande <vedanthande453@gmail.com>
2025-07-24 22:02:51 +0530

    adding a full post functionaliyt

--- Backend/REST/index.js
+++ Backend/REST/index.js
@@ -2,6 +2,7 @@ const { name } = require("ejs");
 const express = require("express");
 const app = express();
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
 const port = 3000;
 
@@ -13,21 +14,25 @@ app.use(express.urlencoded({ extended: true }));
 
 let posts = [
   {
+    id: "a4",
     name: "John Doe",
     title: "My First Post",
     content: "This is the content of my first post.",
   },
   {
+    id: "a3",
     name: "Alice Smith",
     title: "Exploring Node.js",
     content: "Quick tips on Node.js development.",
   },
   {
+    id: "a2",
     name: "Tom Brown",
     title: "Understanding Express.js",
     content: "This is the content of my first post.",
   },
   {
+    id: "a1",
     name: "sarthak sharma",
     title: "html css js",
     content: " This is the content of my first post.",
@@ -40,14 +45,23 @@ app.listen(port, () => {
 
 app.get("/posts", (req, res) => {
   res.render("index.ejs", { posts });
-}); 
+});
 
 app.get("/posts/new", (req, res) => {
   res.render("new.ejs");
 });
 
 app.post("/posts", (req, res) => {
-  let { name, title, content } = req.body;
-  posts.push({ name, title, content });
+  let { id, name, title, content } = req.body;
+  posts.push({ id, name, title, content });
   res.redirect("/posts");
+});
+
+app.get("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => {
+    return id === p.id;
+  });
+  console.log(post);
+  res.render("show.ejs", { post });
 });
