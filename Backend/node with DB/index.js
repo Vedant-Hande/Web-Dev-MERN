3c171060fa89c8c7a61950d3d9eda225c050b3e1
vedant hande <vedanthande453@gmail.com>
2025-07-30 21:57:43 +0530

    Refactor user generation and update database schema to create 'student' table

--- Backend/node with DB/index.js
+++ Backend/node with DB/index.js
@@ -1,41 +1,43 @@
 const { faker } = require("@faker-js/faker");
 const mysql = require("mysql2");
 
-let getRandomUser = () => {
-  return {
-    userId: faker.string.uuid(),
-    username: faker.internet.username(), // before version 9.1.0, use userName()
-    email: faker.internet.email(),
-    registeredAt: faker.date.past(),
-  };
-};
-
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  database: "delta_app", // Uncomment and replace with your actual database name
+  database: "delta_app",
   password: "vedant9699#",
   connectTimeout: 10000, // 10 seconds timeout
 });
+
+let getRandomUser = () => {
+  return {
+    id: faker.string.uuid(),
+    username: faker.internet.username(),
+    email: faker.internet.email(),
+  };
+};
 
 // Handle connection errors
 connection.connect((err) => {
   if (err) {
     console.error("Error connecting to MySQL:", err.message);
-    console.error("Error code:", err.code);
     return;
   }
   console.log("Connected to MySQL successfully!");
 
   // Execute query after successful connection
-  connection.query("SHOW TABLES", (error, results) => {
+
+  let query = "INSERT INTO users (id, username, email) VALUES ()";
+  let user = [getRandomUser];
+  connection.query(query, user, (error, results) => {
     if (error) {
       console.error("Error showing databases:", error.message);
       return;
     }
     console.log("Available databases:", results);
+    console.log(results[0]);
     connection.end(); // Close the connection
   });
 });
 
-console.log(getRandomUser());
+// console.log(getRandomUser());
