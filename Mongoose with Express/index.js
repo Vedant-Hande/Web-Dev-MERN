bd908423005427a81318a1eb9b4298d3e5f12ba0
vedant hande <vedanthande453@gmail.com>
2025-08-21 00:23:49 +0530

    feat: add express import to initialize express app

--- Mongoose with Express/index.js
+++ Mongoose with Express/index.js
@@ -1,8 +1,24 @@
-const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
-
 const port = 3000;
+
 app.listen(port, () => {
-  console.log("server is listening", port);
+  console.log(`server is listening on port ${port}`);
+});
+
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/whatapp");
+}
+
+main()
+  .then((result) => {
+    console.log(`connected to mongoDB`);
+  })
+  .catch((err) => {
+    console.log("error connecting to mongoDB", err);
+  });
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Mongoose with Express app!");
 });
